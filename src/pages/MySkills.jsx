5554f6bcf8c7b5ff43c18ca3c791d21d50b213f7
Vinícius Soares <vinivsp2304@hotmail.com>
2022-11-09 19:16:54 -0300

    Create MySkills.jsx

---
+++ src/pages/MySkills.jsx
@@ -0,0 +1,139 @@
+import { LineGradient } from "../components/LineGradient";
+import { useMediaQuery } from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+export const MySkills = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      <div className="md:flex md:justify-between md:gap-16 mt-32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            MINHAS HABILIDADES <span className="text-red">HABILIDADES</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </motion.div>
+
+        <div className="mt-16 md:mt-0 ">
+          {isAboveMediumScreens ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+            before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                src="assets/skills-image.png"
+                className="z-10"
+              />
+            </div>
+          ) : (
+            <img alt="skills" src="assets/skills-image.png" className="z-10" />
+          )}
+        </div>
+      </div>
+
+      {/* HABILIDADES */}
+
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* EXPERIÊNCIA */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32 ">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">01</p>
+              <p className="font-playfair font-semibold text-5xl mt-3">
+                Experiência
+              </p>
+            </div>
+            <div className="w-1/2 md:-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+          </p>
+        </motion.div>
+
+        {/* INOVAÇÃO */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 1 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32 ">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">02</p>
+              <p className="font-playfair font-semibold text-5xl mt-3">
+                Inovativa
+              </p>
+            </div>
+            <div className="w-1/2 md:-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+          </p>
+        </motion.div>
+
+        {/* IMAGINATIVA */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.7, duration: 1 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32 ">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">02</p>
+              <p className="font-playfair font-semibold text-5xl mt-3">
+                Imaginativa
+              </p>
+            </div>
+            <div className="w-1/2 md:-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
