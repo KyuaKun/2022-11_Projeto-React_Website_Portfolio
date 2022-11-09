25a7b71ffc1194d49d8cd7c51dc0de6aaff84e2e
Vinícius Soares <vinivsp2304@hotmail.com>
2022-11-09 18:34:33 -0300

    Create SocialMediaIcons

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,41 @@
+export const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com"
+        target="_blank"
+        rel="nonreferrer"
+      >
+        <img alto="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="nonreferrer"
+      >
+        <img alto="facebook-link" src="../assets/facebook.png" />
+      </a>
+
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="nonreferrer"
+      >
+        <img alto="instagram-link" src="../assets/instagram.png" />
+      </a>
+
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="nonreferrer"
+      >
+        <img alto="twitter-link" src="../assets/twitter.png" />
+      </a>
+    </div>
+  );
+};
