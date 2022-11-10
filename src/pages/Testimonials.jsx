a3922f051e7fda11242037ef69dee765135de8a4
Vinícius Soares <vinivsp2304@hotmail.com>
2022-11-10 15:55:26 -0300

    Update Testimonials.jsx

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -2,18 +2,15 @@ import { LineGradient } from "../components/LineGradient";
 import { motion } from "framer-motion";
 
 export const Testimonials = () => {
-  const testimonialsStyles = `mx-auto relative max-w-[400px] flex flex-col justify-end
-     pt-16 mt-48 before:absolute before:top-[120px] before:-ml-[110px] before:left-1/2 `;
-
   return (
-    <section id="depoimentos" className="pt-32 pb-16">
+    <section id="testimonials" className="pt-32 pb-16">
       {/* HEADING */}
       <motion.div
         className="md:w-1/3 text-center md:text-left"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, amount: 0.5 }}
-        transition={{ duration: 1 }}
+        transition={{ duration: 0.5 }}
         variants={{
           hidden: { opacity: 0, x: -50 },
           visible: { opacity: 1, x: 0 },
@@ -22,64 +19,66 @@ export const Testimonials = () => {
         <p className="font-playfair font-semibold text-4xl mb-5 text-red">
           DEPOIMENTOS
         </p>
-        <LineGradient width="mx-auto w-2/4" />
+        <LineGradient width="mx-auto w-2/5" />
         <p className="mt-10">
-          Aqui estão alguns depoimentos sobre o meu trabalho: Lorem ipsum dolor
-          sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua.
+          Aqui estão alguns depoimentos de pessoas que contraram o meu serviço
         </p>
       </motion.div>
 
-      {/* DEPOIMENTOS */}
-      <div className="md:flex md:justify-between gap-8 ">
+      {/* TESTIMONIALS */}
+      <div className="md:flex md:justify-between gap-8">
         <motion.div
-          className={`bg-blue ${testimonialsStyles} before:content-person1`}
+          className="mx-auto relative bg-blue max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+              before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person1"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 1 }}
+          transition={{ duration: 0.6 }}
           variants={{
             hidden: { opacity: 0, scale: 0.8 },
             visible: { opacity: 1, scale: 1 },
           }}
         >
-          <p className="font-playfair text-6xl"></p>
+          <p className="font-playfair text-6xl">“</p>
           <p className="text-center text-xl">
-            Sodales ut eu sem integer vitae justo eget.
+            A auctor pharetra hendrerit mattis amet etiam interdum platea.
           </p>
         </motion.div>
 
         <motion.div
-          className={`bg-yellow ${testimonialsStyles} before:content-person2`}
+          className="mx-auto relative bg-red max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+              before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person2"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.5, duration: 1 }}
+          transition={{ delay: 0.2, duration: 0.6 }}
           variants={{
             hidden: { opacity: 0, scale: 0.8 },
             visible: { opacity: 1, scale: 1 },
           }}
         >
-          <p className="font-playfair text-6xl"></p>
+          <p className="font-playfair text-6xl">“</p>
           <p className="text-center text-xl">
-            Nulla porttitor massa id neque aliquam vestibulum morbi blandit.
+            Aliquam aliquet integer ut fames odio in at. At magna ornare dictum
+            lectus.
           </p>
         </motion.div>
 
         <motion.div
-          className={`bg-red ${testimonialsStyles} before:content-person3`}
+          className="mx-auto relative bg-yellow max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+              before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person3"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 1, duration: 1 }}
+          transition={{ delay: 0.4, duration: 0.6 }}
           variants={{
             hidden: { opacity: 0, scale: 0.8 },
             visible: { opacity: 1, scale: 1 },
           }}
         >
-          <p className="font-playfair text-6xl"></p>
+          <p className="font-playfair text-6xl">“</p>
           <p className="text-center text-xl">
-            Nulla porttitor massa id neque aliquam vestibulum morbi blandit.
+            Fames odio in at. At magna ornare dictum lectus.
           </p>
         </motion.div>
       </div>
