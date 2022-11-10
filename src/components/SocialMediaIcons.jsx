7e698941d37a78e0fabee1d8e0e46c06d92da0d6
Vinícius Soares <vinivsp2304@hotmail.com>
2022-11-10 12:26:14 -0300

    Update SocialMediaIcons.jsx

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -5,36 +5,36 @@ export const SocialMediaIcons = () => {
         className="hover:opacity-50 transition duration-500"
         href="https://www.linkedin.com"
         target="_blank"
-        rel="nonreferrer"
+        rel="noreferrer"
       >
-        <img alto="linkedin-link" src="../assets/linkedin.png" />
+        <img alt="linkedin-link" src="../assets/linkedin.png" />
       </a>
 
       <a
         className="hover:opacity-50 transition duration-500"
         href="https://www.facebook.com"
         target="_blank"
-        rel="nonreferrer"
+        rel="noreferrer"
       >
-        <img alto="facebook-link" src="../assets/facebook.png" />
+        <img alt="facebook-link" src="../assets/facebook.png" />
       </a>
 
       <a
         className="hover:opacity-50 transition duration-500"
         href="https://www.instagram.com"
         target="_blank"
-        rel="nonreferrer"
+        rel="noreferrer"
       >
-        <img alto="instagram-link" src="../assets/instagram.png" />
+        <img alt="instagram-link" src="../assets/instagram.png" />
       </a>
 
       <a
         className="hover:opacity-50 transition duration-500"
         href="https://www.twitter.com"
         target="_blank"
-        rel="nonreferrer"
+        rel="noreferrer"
       >
-        <img alto="twitter-link" src="../assets/twitter.png" />
+        <img alt="twitter-link" src="../assets/twitter.png" />
       </a>
     </div>
   );
